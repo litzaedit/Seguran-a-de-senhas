bb1fa23a883c1259524547df05ff93dcdb3cd65d
mariana <103067658+litzaedit@users.noreply.github.com>
2024-05-29 09:14:52 -0300

    29 05 24

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ let tamanhoSenha = 5;
 numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
+
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
+
 function diminuiTamanho(){
     if (tamanhoSenha > 1){
        // tamanhoSenha = tamanhoSenha-1;
